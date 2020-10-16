e6e24ee1ac1134a51a46aea81e790687707d1173
mr <mr@outs.pl>
2020-10-16 23:19:27 +0200

    order book stat generation part

---
+++ bdata_stat.py
@@ -0,0 +1,47 @@
+import time
+
+from sqlalchemy import text
+
+from bdata_db import Session, engine
+
+
+def make_stat():
+    while True:
+        with engine.connect() as con:
+            con.execute(text("""
+do
+$$
+    declare
+        an   int[] := array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+        n    int;
+        bsid bigint;
+        code varchar(20);
+    begin
+        for i in 1..10
+            loop
+                bsid := (select max(book_snap_id) from book_snap where stat = false);
+                if bsid is null
+                then
+                    raise notice 'nothing to do';
+                    return;
+                end if;
+                raise notice 'bsid=%', bsid;
+                delete from book_snap_stat where book_snap_id = bsid;
+                foreach n in array an
+                    loop
+                        code := 'r' || n::text;
+                        insert into book_snap_stat(book_snap_id, code, data) select bsid, code, bookSnapStat(bsid, n);
+                        --raise notice 'code=%', code;
+                    end loop;
+                update book_snap set stat = true where book_snap_id = bsid;
+            end loop;
+        delete from book_snap_bid where book_snap_id = bsid;
+        delete from book_snap_ask where book_snap_id = bsid;
+    end
+$$;
+            """))
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    make_stat()
