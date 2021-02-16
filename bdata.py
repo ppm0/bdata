734978a334821ea3cac7cc80723cbb7f4cfa3354
mr <mr@outs.pl>
2021-02-16 21:27:38 +0100

    loop goes to infinite due

--- bdata.py
+++ bdata.py
@@ -46,19 +46,28 @@ DISABLED_EXCHANGES = ['bitfinex2', 'anxpro', 'bcex', 'vaultoro', 'coss', 'coolco
                       'okex3',  # use only okex
                       'fcoin', 'fcoinjp',  # 20200220 exit scam
                       'bitbay',  # fucked up
-                      '_1btcxe',  # error - ddos protection
-                      # ExchangeError bitget {"status":"error","ts":1612883459971,"err_code":"invalid-parameter","err_msg":"Failed to convert property value of type \u0027java.lang.String\u0027 to required type \u0027java.lang.Integer\u0027 for property \u0027size\u0027; nested exception is java.lang.NumberFormatException: For input string: \"1612828800000\""}
+                      '_1btcxe',
+
+                      # error - ddos protection ExchangeError bitget {"status":"error",
+                      # "ts":1612883459971,"err_code":"invalid-parameter","err_msg":"Failed to convert property value
+                      # of type \u0027java.lang.String\u0027 to required type \u0027java.lang.Integer\u0027 for
+                      # property \u0027size\u0027; nested exception is java.lang.NumberFormatException: For input
+                      # string: \"1612828800000\""}
                       'bitget',
-                      # DDoSProtection bytetrade 503 Service Unavailable {"code":1,"msg":"Please use websocket to get this data."}
+
+                      # DDoSProtection bytetrade 503 Service Unavailable {"code":1,"msg":"Please use websocket to get
+                      # this data."}
                       'bytetrade',
-                      # BTC/USDT BadSymbol eterbase {"message":"Invalid market !","_links":{"self":{"href":"/markets/3/trades","templated":false}}}
+
+                      # BTC/USDT BadSymbol eterbase {"message":"Invalid market !","_links":{"self":{
+                      # "href":"/markets/3/trades","templated":false}}}
                       'eterbase',
                       ]
 
 
 def decimalize(book):
-    return {'bids': [(Decimal(str(l[0])), Decimal(str(l[1]))) for l in book['bids']],
-            'asks': [(Decimal(str(l[0])), Decimal(str(l[1]))) for l in book['asks']]
+    return {'bids': [(Decimal(str(b[0])), Decimal(str(b[1]))) for b in book['bids']],
+            'asks': [(Decimal(str(a[0])), Decimal(str(a[1]))) for a in book['asks']]
             }
 
 
@@ -107,7 +116,7 @@ def ensure_exchange_market(session, exchange, base, quote) -> Tuple[Exchange, Ex
         session.add(em)
         session.commit()
 
-    return (e, em, bt, qt)
+    return e, em, bt, qt
 
 
 def snap_book(mts: datetime.datetime, exchange: ccxt.Exchange, base: str, quote: str):
@@ -170,10 +179,10 @@ def snap_trades(ts: datetime.datetime, exchange: ccxt.Exchange, base: str, quote
 
             trades_all = []
             market = base + '/' + quote
-
+            max_ts = exchange.milliseconds()
             trades_prior = None
             if exchange.id.startswith(BINANCE):
-                while since < exchange.milliseconds() and len(trades_all) < TRADES_LIMIT:
+                while since < max_ts and len(trades_all) < TRADES_LIMIT:
                     trades_tmp = exchange.fetch_trades(symbol=market, since=since)
                     if json.dumps(trades_prior, sort_keys=True) == json.dumps(trades_tmp, sort_keys=True):
                         since += 55 * 60 * 1000
@@ -185,7 +194,7 @@ def snap_trades(ts: datetime.datetime, exchange: ccxt.Exchange, base: str, quote
                             since += 55 * 60 * 1000
                     trades_prior = deepcopy(trades_tmp)
             else:
-                while since < exchange.milliseconds() and len(trades_all) < TRADES_LIMIT:
+                while since < max_ts and len(trades_all) < TRADES_LIMIT:
                     trades_tmp = exchange.fetch_trades(market, since)
                     if len(trades_tmp) > 0:
                         if last_eid == trades_tmp[-1]['id']:
@@ -227,6 +236,7 @@ def snap_trades(ts: datetime.datetime, exchange: ccxt.Exchange, base: str, quote
 
             session.commit()
 
+            logging.info('{}::{} end'.format(exchange.id, market))
         except:
             session.rollback()
             raise
@@ -234,22 +244,44 @@ def snap_trades(ts: datetime.datetime, exchange: ccxt.Exchange, base: str, quote
         session.close()
 
 
+RETRIES = 3
+
+
 def snap(exchange: ccxt.Exchange, market: dict, ts: datetime, snap_target: SnapTarget):
-    try:
-        if snap_target in [SnapTarget.ALL, SnapTarget.BOOK]:
-            snap_book(ts, exchange, market['base'], market['quote'])
-        if snap_target in [SnapTarget.ALL, SnapTarget.TRADE]:
-            snap_trades(ts, exchange, market['base'], market['quote'])
-    except Exception as e:
-        logging.error(
-            '{}::{} {} {}'.format(exchange.id, market['symbol'], sys.exc_info()[0].__name__, sys.exc_info()[1]))
+    if snap_target in [SnapTarget.ALL, SnapTarget.BOOK]:
+        c = 0
+        while c < RETRIES:
+            try:
+                snap_book(ts, exchange, market['base'], market['quote'])
+                break
+            except:
+                logging.error(
+                    '{}::{} {} {}'.format(exchange.id, market['symbol'], sys.exc_info()[0].__name__,
+                                          sys.exc_info()[1]))
+                c = c + 1
+                if c < RETRIES:
+                    time.sleep(5)
+
+    if snap_target in [SnapTarget.ALL, SnapTarget.TRADE]:
+        c = 0
+        while c < RETRIES:
+            try:
+                snap_trades(ts, exchange, market['base'], market['quote'])
+                break
+            except:
+                logging.error(
+                    '{}::{} {} {}'.format(exchange.id, market['symbol'], sys.exc_info()[0].__name__,
+                                          sys.exc_info()[1]))
+                c = c + 1
+                if c < RETRIES:
+                    time.sleep(5)
 
 
 def market_filter(market) -> bool:
     global base_list, quote_list
     return market['base'] and market['quote'] and \
-           (market['base'] in base_list or base_list[0] == '*') and \
-           (market['quote'] in quote_list or quote_list[0] == '*') and '/' in market['symbol']
+        (market['base'] in base_list or base_list[0] == '*') and \
+        (market['quote'] in quote_list or quote_list[0] == '*') and '/' in market['symbol']
 
 
 def exchange_filter(exchange) -> bool:
